5996558d56fa84d4470afa37b307773eed1ef336
Jasjot Singh <jasjot784@gmail.com>
2019-09-13 13:55:11 +0530

    get and diplay array elements

---
+++ display.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[5];
+  cout<<"enter 5 elements"<<endl;
+  for(int i=0;i<5;i++)
+  {
+    cin>>arr[i];
+  }
+  cout<<"elemnts entered are"<<endl;
+  for(int i=0;i<5;i++)
+  {
+    cout<<arr[i]<<endl;
+  }
+}
